6521db27432f9f341ed6836ab9806e454beaf317
Gijs <gijsschroder@gmail.com>
2024-06-13 17:59:54 +0200

    Trim should test for printable characters instead; include spaces

--- wfsms/trim.hpp
+++ wfsms/trim.hpp
@@ -9,14 +9,14 @@
 // trim from start (in place)
 static inline void ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
+        return std::isprint(ch);
     }));
 }
 
 // trim from end (in place)
 static inline void rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
+        return std::isprint(ch);
     }).base(), s.end());
 }
 
